730c62dc6be39550e32c60870b929593d7134598
Frederick <riki_email@yahoo.com>
2015-10-12 12:51:47 -0400

    new function

--- database.js
+++ database.js
@@ -6,6 +6,14 @@ var connectionString = process.env.DATABASE_URL || 'postgres://postgres:1@localh
 var db = pgp(connectionString);
 var dbConn = "asd riki"
 
+var rikiModified = function(){
+
+	return function(a,b){
+		a= 1+ b
+		return "Value of a is = " + a;
+
+	}
+}
 
 db.query("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(255) not null, password VARCHAR(255) not null)")
 .then(function (data) {
