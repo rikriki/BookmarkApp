a03edd3bf18a620716bd98b8898309eee19ad296
Frederick <riki_email@yahoo.com>
2015-10-12 12:44:03 -0400

    first

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ var pgp = require('pg-promise')();
 var connectionString = process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5432/todo';
 var db = pgp(connectionString);
 
-
+var dbConn = "asd"
 
 
 db.query("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(255) not null, password VARCHAR(255) not null)")
